b15d52098a7f40727f44b135102e2791e4d427b2
Brian Hogg <brian@bhconsulting.ca>
2011-09-18 23:39:33 -0400

    Experimenting with function to get all public taxnomies (custom or otherwise) and generate the default feed url Refs #37

--- templates/admin/tabs/list.php
+++ templates/admin/tabs/list.php
@@ -132,7 +132,31 @@ $tabsUnpublished = 0;
 
     	<div id="<?php echo $row->component . 'Tab' ?>">
 
+
     	<?php echo '<p>TEMPLATE</p>'; //echo $weeverapp->loadTemplate($row->component.'dropdown'); ?>
+		<?php foreach ( get_taxonomies( array( 'public' => true, 'show_in_nav_menus' => true ), 'objects' ) as $taxonomy ): ?>
+			<h3><?php echo $taxonomy->label; ?></h3>
+        	<select name="cms_feed">
+        	<?php foreach ( get_terms( $taxonomy->name ) as $term ): ?>
+			<option value="<?php
+			    $feed = 'r3s';
+			    $term_id = $term->term_id;
+			    $taxonomy = $term->taxonomy;
+				if ( 'category' == $taxonomy ) {
+        			$link = home_url("?feed=$feed&amp;cat=$term_id");
+        		}
+        		elseif ( 'post_tag' == $taxonomy ) {
+        			$link = home_url("?feed=$feed&amp;tag=$term->slug");
+        		} else {
+        			$t = get_taxonomy( $taxonomy );
+        			$link = home_url("?feed=$feed&amp;$t->query_var=$term->slug");
+        		}
+                  echo $link;
+
+			//echo get_term_feed_link( $term ); ?>"><?php echo $term->name; ?></option>
+        	<?php endforeach; ?>
+    		</select>
+		<?php endforeach; ?>
 
     	<input type="hidden" name="boxchecked<?php echo $row->component; ?>" id="boxchecked<?php echo $row->component; ?>" value="0" />
     	<table class='adminlist'>
