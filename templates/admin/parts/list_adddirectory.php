e28dbe2d7409a005d105c987107e5bff1ce817aa
Brian Hogg <brian@bhconsulting.ca>
2011-12-08 11:38:21 -0500

    Ability to specify image url, but need to be able to save the url server-side to be rendered into the app [#22007641]

--- templates/admin/parts/list_adddirectory.php
+++ templates/admin/parts/list_adddirectory.php
@@ -17,6 +17,8 @@
 			<?php endforeach; ?>
 			<option value="s"><?php echo __( 'From a Search Term', 'weever' ); ?></option>
 		</select>
+		
+		<label for="wx-select-directory"><a target="_blank" href="http://weeverapps.zendesk.com/entries/20673217-what-is-the-directory-tab-for">Learn More About the Directory Tab</a></label>
 	</div>
 
 	<div class='wx-dummy wx-directory-dummy'>
@@ -45,7 +47,7 @@
     		<input type='text' value='' id='wx-add-directory-s-input' class='wx-input wx-directory-input' name='s' placeholder='<?php echo __( 'Search Term', 'weever' ); ?>' />
     		<label for='wx-add-directory-s-input' id='wx-add-directory-s-input-label' class='wx-directory-label'><?php echo __( 'Search Term Description', 'weever' ); ?></label>
 		</div>
-
+		
 	</div>
 
 	<div class='wx-add-title wx-directory-reveal wx-reveal'>
@@ -54,6 +56,12 @@
 		<label for='wx-directory-title'><?php echo __( 'Submenu Tab Name/Description', 'weever' ); ?></label>
 	</div>
 
+	<div class='wx-add-title wx-directory-reveal wx-reveal'>
+
+		<input type='text' id='wx-directory-image-url' value='' class='wx-title wx-input wx-directory-image-url' placeholder='http://' name='noname' />
+		<label for='wx-directory-image-url'><?php echo __( 'Optional Image URL', 'weever' ); ?></label>
+	</div>
+	
 	<div class='wx-add-submit'>
 		<input type='submit' id='wx-directory-submit' class='wx-submit' value='<?php echo __( 'Submit', 'weever' ); ?>' name='add' />
 	</div>
